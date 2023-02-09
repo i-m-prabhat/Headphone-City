41bc205d885f1714e172bb168b8d6e9d7180a357
Prabhat Patel <prabhatpatel508@outlook.com>
2023-02-09 16:30:02 +0530

    home category section completed

--- client/src/components/Home/Category/Category.jsx
+++ client/src/components/Home/Category/Category.jsx
@@ -1,6 +1,24 @@
+import React from "react";
+import cat1 from "../../../assets/category/cat-1.jpg"
 import "./Category.scss";
+
 const Category = () => {
-    return <div>Category</div>;
+    return <div className="shop-by-category">
+        <div className="categories">
+            <div className="category">
+                <img src={cat1} alt="category img" />
+            </div>
+            <div className="category">
+                <img src={cat1} alt="category img" />
+            </div>
+            <div className="category">
+                <img src={cat1} alt="category img" />
+            </div>
+            <div className="category">
+                <img src={cat1} alt="category img" />
+            </div>
+        </div>
+    </div>;
 };
 
 export default Category;
--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,8 +1,15 @@
 import Banner from "./Banner/Banner";
+import Category from "./Category/Category";
 import "./Home.scss";
-const Home = () => {
-    return <div className="home">
-        <Banner/>
+const Home = () =>
+{
+    return <div>
+        <Banner />
+        <div className="main-content">
+            <div className="layout">
+                <Category />
+            </div>
+        </div>
     </div>;
 };
 
