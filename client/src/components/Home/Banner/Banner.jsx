7c98baa5f1b301b09f5bbd3bcac5a1fa74815cc1
Prabhat Patel <prabhatpatel508@outlook.com>
2023-02-09 11:49:32 +0530

    banner Section hero-section completed

--- client/src/components/Home/Banner/Banner.jsx
+++ client/src/components/Home/Banner/Banner.jsx
@@ -1,6 +1,20 @@
 import "./Banner.scss";
+import BannerImg from "../../../assets/banner-img.png"
+
 const Banner = () => {
-    return <div>Banner</div>;
+    return <div className="hero-banner">
+        <div className="content">
+            <div className="text-content">
+                <h1>SALES</h1>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse incidunt aperiam mollitia deleniti? Velit voluptatibus quis tempore atque. Fugiat vitae autem est mollitia at dicta soluta magnam eius repudiandae ullam.</p>
+                <div className="ctas">
+                    <div className="banner-cta">Read More</div>
+                    <div className="banner-cta v2">Shop Now</div>
+                </div>
+            </div>
+            <img className="banner-img" src={BannerImg} alt="Banner img" />
+        </div>
+    </div>;
 };
 
 export default Banner;
--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,6 +1,9 @@
+import Banner from "./Banner/Banner";
 import "./Home.scss";
 const Home = () => {
-    return <div className="home">Home</div>;
+    return <div className="home">
+        <Banner/>
+    </div>;
 };
 
 export default Home;
