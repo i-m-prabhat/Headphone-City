65583685eebfe37b2620714b10a284ebd3b4c1ef
Prabhat Patel <prabhatpatel508@outlook.com>
2023-03-13 22:06:56 +0530

    api

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -2,8 +2,22 @@ import Products from "../Products/Products";
 import Banner from "./Banner/Banner";
 import Category from "./Category/Category";
 import "./Home.scss";
+import { useEffect } from "react";
+import { fetchDataFromApi } from "../../utils/api";
+
+
 const Home = () =>
 {
+    useEffect(()=>{
+        getCategories();
+    },[]);
+
+    const getCategories = () => {
+        fetchDataFromApi("api/categories?populate=*").then((res) => {
+            console.log(res);
+        })
+    }
+
     return <div>
         <Banner />
         <div className="main-content">
