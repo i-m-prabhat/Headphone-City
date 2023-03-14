21ba94c8899a8a8993f8b8ffd55c37bc50e96acf
Prabhat Patel <prabhatpatel508@outlook.com>
2023-03-14 20:23:47 +0530

    useFetch custom hook created

---
+++ client/src/hooks/useFetch.js
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+import { fetchDataFromApi } from "../utils/api"
+
+const useFetch = (endpoint) =>
+{
+    const [data, setData] = useState()
+
+    useEffect(async () =>
+    {
+        makeApiCall()
+    }, [endpoint])
+
+    const makeApiCall = async () =>
+    {
+        const res = await fetchDataFromApi(endpoint)
+        setData(res)
+    }
+    return data
+}
+
+export default useFetch;
