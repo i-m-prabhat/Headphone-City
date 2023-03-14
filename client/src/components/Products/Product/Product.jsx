3efd73608e223add28656d40fa75b60fd9e33b8d
Prabhat Patel <prabhatpatel508@outlook.com>
2023-03-14 20:00:09 +0530

    product api complete

--- client/src/components/Products/Product/Product.jsx
+++ client/src/components/Products/Product/Product.jsx
@@ -1,15 +1,15 @@
 import React from "react";
-import prod from "../../../assets/products/earbuds-prod-1.webp";
+// import prod from "../../../assets/products/earbuds-prod-1.webp";
 import "./Product.scss";
-const Product = () =>
+const Product = ({ id, data }) =>
 {
     return <div className="product-card">
         <div className="thumbnail">
-            <img src={prod} alt="product thumbnail" />
+            <img src={process.env.REACT_APP_DEV_URL + data.img.data[0].attributes.url} alt="product thumbnail" />
         </div>
         <div className="prod-details">
-            <span className="name">Product name</span>
-            <div className="price">&#8377;499</div>
+            <span className="name">{data.title}</span>
+            <div className="price">&#8377; {data.price}</div>
         </div>
     </div>;
 };
--- client/src/components/Products/Products.jsx
+++ client/src/components/Products/Products.jsx
@@ -1,19 +1,19 @@
 import React from "react";
 import Product from "./Product/Product"
 import "./Products.scss";
-const Products = ({innerPage,headingText}) =>
+const Products = ({ innerPage, headingText, products }) =>
 {
     return <div className="products-container">
         {!innerPage && <div className="sec-heading">{headingText}</div>}
         <div className="products">
-            <Product />
-            <Product />
-            <Product />
-            <Product />
-            <Product />
-            <Product />
-            <Product />
-            <Product />
+            {products.data.map((item) => (
+                <Product
+                    key={item.id}
+                    id={item.id}
+                    data={item.attributes}
+                />
+            ))}
+
         </div>
     </div>;
 };
