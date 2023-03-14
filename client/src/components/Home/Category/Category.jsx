7319c348c26b6475168139d0441828df1a42cbce
Prabhat Patel <prabhatpatel508@outlook.com>
2023-03-14 20:41:22 +0530

    products category api data added

--- client/src/components/Home/Category/Category.jsx
+++ client/src/components/Home/Category/Category.jsx
@@ -1,13 +1,18 @@
+import { useNavigate } from "react-router-dom"
+
 import React from "react";
 // import cat1 from "../../../assets/category/cat-1.jpg"
 import "./Category.scss";
 
 const Category = ({ categories }) =>
 {
+    const navigate = useNavigate();
     return <div className="shop-by-category">
         <div className="categories">
-            {categories.data.map((item) => (
-                <div key={item.id} className="category">
+            {categories?.data?.map((item) => (
+                <div key={item.id}
+                    className="category"
+                    onClick={() => navigate(`/category/${item.id}`)}>
                     <img src={process.env.REACT_APP_DEV_URL + item.attributes.img.data.attributes.url} alt="category img" />
                 </div>
             ))}
--- client/src/components/Products/Products.jsx
+++ client/src/components/Products/Products.jsx
@@ -6,7 +6,7 @@ const Products = ({ innerPage, headingText, products }) =>
     return <div className="products-container">
         {!innerPage && <div className="sec-heading">{headingText}</div>}
         <div className="products">
-            {products.data.map((item) => (
+            {products?.data?.map((item) => (
                 <Product
                     key={item.id}
                     id={item.id}
