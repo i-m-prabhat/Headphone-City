c64f40d038b85fa1df63d3cf7391da269d66fb1a
Prabhat Patel <prabhatpatel508@outlook.com>
2023-03-14 21:58:28 +0530

    related products added

--- client/src/components/SingleProduct/RelatedProducts/RelatedProducts.jsx
+++ client/src/components/SingleProduct/RelatedProducts/RelatedProducts.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 import Products from "../../Products/Products"
-const RelatedProducts = () => {
+import useFetch from "../../../hooks/useFetch";
+import Product from "../../Products/Product/Product";
+
+const RelatedProducts = ({ categoryId, productId }) =>
+{
+    const { data } = useFetch(`/api/products?populate=*&filters[id][$ne]=${productId}&filters[categories][id]=${categoryId}&pagination[start]=0&pagination[limit]=4`)
     return <div>
-        <Products headingText="Related Products"/>
+        <Products headingText="Related Products" products={data} />
     </div>;
 };
 
--- client/src/components/SingleProduct/SingleProduct.jsx
+++ client/src/components/SingleProduct/SingleProduct.jsx
@@ -86,7 +86,10 @@ const SingleProduct = () =>
                     </div>
                 </div>
             </div>
-            <RelatedProducts />
+            <RelatedProducts
+                productId={id}
+                categoryId={product.categories.data[0].id}
+            />
         </div>
     </div>;
 };
