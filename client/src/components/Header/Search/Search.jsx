483b25d00b54eed42f3fd80f619e7f7fe251eee5
Prabhat Patel <prabhatpatel508@outlook.com>
2023-03-15 20:06:42 +0530

    search result

--- client/src/components/Header/Search/Search.jsx
+++ client/src/components/Header/Search/Search.jsx
@@ -39,7 +39,11 @@ const Search = ({ setShowSearch }) =>
         <div className="search-result-content">
             <div className="search-results">
                 {data?.data?.map(item => (
-                    <div key={item.id} className="search-result-item">
+                    <div key={item.id} className="search-result-item" onClick={() =>
+                    {
+                        navigate("/product/" + item.id)
+                        setShowSearch(false);
+                    }}>
                         <div className="img-container">
                             <img src={
                                 process.env.REACT_APP_DEV_URL + item.attributes.img.data[0].attributes.url
