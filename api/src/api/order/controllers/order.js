ff3d941b9980fc4a208753c51e2eefd5dd6c98c6
Prabhat Patel <prabhatpatel508@outlook.com>
2023-03-15 20:28:15 +0530

    stripe payment gateway

---
+++ api/src/api/order/controllers/order.js
@@ -0,0 +1,55 @@
+("use strict");
+const stripe = require("stripe")(process.env.STRIPE_KEY);
+/**
+ * order controller
+ */
+
+const { createCoreController } = require("@strapi/strapi").factories;
+
+module.exports = createCoreController("api::order.order", ({ strapi }) => ({
+    async create(ctx)
+    {
+        const { products } = ctx.request.body;
+        try
+        {
+            const lineItems = await Promise.all(
+                products.map(async (product) =>
+                {
+                    const item = await strapi
+                        .service("api::product.product")
+                        .findOne(product.id);
+
+                    return {
+                        price_data: {
+                            currency: "inr",
+                            product_data: {
+                                name: item.title,
+                            },
+                            unit_amount: Math.round(item.price * 100),
+                        },
+                        quantity: product.attributes.quantity,
+                    };
+                })
+            );
+
+            const session = await stripe.checkout.sessions.create({
+                shipping_address_collection: { allowed_countries: ["IN"] },
+                payment_method_types: ["card"],
+                mode: "payment",
+                success_url: process.env.CLIENT_URL + "/success",
+                cancel_url: process.env.CLIENT_URL + "?success=false",
+                line_items: lineItems,
+            });
+
+            await strapi
+                .service("api::order.order")
+                .create({ data: { products, stripeId: session.id } });
+
+            return { stripeSession: session };
+        } catch (error)
+        {
+            ctx.response.status = 500;
+            return { error };
+        }
+    },
+}));
